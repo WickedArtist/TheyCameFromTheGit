9c26863946a196876c56e40f84400920873c5399
WickedArtist <71231933+WickedArtist@users.noreply.github.com>
2020-09-19 02:15:36 -0400

    more menu fiddlin

---
+++ SlamBall/Assets/Scripts/PlayGameMode1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGameMode1 : MonoBehaviour
+{
+
+    public void PlayGame1()
+    {
+
+    }
+
+
+}
