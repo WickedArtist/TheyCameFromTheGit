51999fa1bef3d5de1f55f0da2ad58987774d6926
NickRatzlaff <48451793+NickRatzlaff@users.noreply.github.com>
2020-09-18 22:01:18 -0400

    pee

--- SlamBall/Assets/Scripts/Player.cs
+++ SlamBall/Assets/Scripts/Player.cs
@@ -7,35 +7,38 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb2d;
     public float movespeed;
     public float jumpForce;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 1.5f;
+
+    public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        movespeed = 5000;
+        movespeed = 30;
         jumpForce = 5000;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("space") && isGrounded)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+            Debug.Log("jump");
+        }
     }
 
     private void FixedUpdate()
     {
         if (Input.GetKey("a"))
         {
-            rb2d.AddForce((Vector2.left * movespeed * Time.fixedDeltaTime));
+            rb2d.transform.Translate((Vector2.left * movespeed * Time.fixedDeltaTime));
         }
         if (Input.GetKey("d"))
         {
-            rb2d.AddForce((Vector2.right * movespeed * Time.fixedDeltaTime));     
-        }
-        if (Input.GetKeyDown("space"))
-        {
-            rb2d.AddForce(Vector2.up * jumpForce);
-            Debug.Log("jump");
+            rb2d.transform.Translate((Vector2.right * movespeed * Time.fixedDeltaTime));     
         }
     }
 }
